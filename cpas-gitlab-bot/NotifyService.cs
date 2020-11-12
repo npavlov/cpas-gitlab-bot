d08132521b8f4b6928208e99ef2e8566f37efffc
Nikita Pavlov <nppavlov@alfabank.ru>
2020-11-12 13:29:22 +0300

    feat: initial release

---
+++ cpas-gitlab-bot/NotifyService.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using NCrontab;
+using Newtonsoft.Json;
+using RestSharp;
+using RestSharp.Authenticators;
+using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
+
+namespace cpas_gitlab_bot
+{
+    public class NotifyService : BackgroundService
+    {
+        private CrontabSchedule _schedule;
+        private DateTime _nextRun;
+
+        private string Schedule => "0 9 * * 1-5"; 
+
+        public NotifyService()
+        {
+            _schedule = CrontabSchedule.Parse(Schedule);
+            _nextRun = _schedule.GetNextOccurrence(DateTime.Now);
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            do
+            {
+                var now = DateTime.Now;
+                var nextrun = _schedule.GetNextOccurrence(now);
+                if (nextrun > _nextRun)
+                {
+                    Process();
+                    _nextRun = _schedule.GetNextOccurrence(DateTime.Now);
+                }
+                await Task.Delay(5000, stoppingToken); //5 seconds delay
+            }
+            while (!stoppingToken.IsCancellationRequested);
+        }
+
+        private void Process()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            var bot_token = Environment.GetEnvironmentVariable("bot_token");
+            var gitlab_token = Environment.GetEnvironmentVariable("gitlab_token");
+            var team = Environment.GetEnvironmentVariable("team");
+
+            TelegramBotClient Bot = new TelegramBotClient(bot_token);
+
+
+            var client = new RestClient("http://gitlab.k8s.alfa.link/api/v4/")
+            {
+                Authenticator = new JwtAuthenticator(gitlab_token)
+            };
+
+            var users = team.Split(';');
+
+
+            MergeRequest[] getMRs(string userId)
+            {
+                var request = new RestRequest($"merge_requests?state=opened&scope=all&per_page=100&author_username={userId}", DataFormat.Json);
+
+                var response = client.Get(request);
+
+
+                return JsonConvert.DeserializeObject<MergeRequest[]>(response.Content);
+            }
+
+            var mergeRequests = new List<MergeRequest>();
+
+            Array.ForEach(users, user =>
+            {
+                mergeRequests.AddRange(getMRs(user.ToLower()));
+            });
+
+            var sr = new StringBuilder();
+
+            mergeRequests.GroupBy(mr => mr.Author.Name).Select(g => new { name = g.Key, mrs = g.ToList() }).ToList().ForEach(group =>
+            {
+
+                sr.AppendLine("");
+                sr.AppendLine($"*{group.name}*");
+
+                group.mrs.ForEach(mr =>
+                {
+                    sr.AppendLine("");
+                    sr.AppendLine($"*Название:*             {mr.Title}");
+                    sr.AppendLine($"*Когда обновлялся:*     {mr.UpdatedAt.ToString("dd-MM-yyyy")}");
+                    sr.AppendLine($"*Дней с обновления:*    {(int)(DateTime.Now - mr.UpdatedAt).TotalDays}");
+                    sr.AppendLine($"*WIP:*                  {mr.WorkInProgress}");
+                    sr.AppendLine($"*Проект:*               {(mr.ProjectId == 452 ? "AKIT" : "ASSR")}");
+                    sr.AppendLine($"*Конфликты:*            {mr.HasConflicts}");
+                    sr.AppendLine($"[ссылка]({mr.WebUrl})");
+                });
+            });
+
+            Bot.SendTextMessageAsync("-345821829", sr.ToString(), parseMode: ParseMode.Markdown).Wait();
+
+            Console.WriteLine(sr.ToString());
+        }
+    }
+}
---
+++ cpas-gitlab-bot/models.cs
@@ -0,0 +1,188 @@
+﻿
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace cpas_gitlab_bot
+{
+    public partial class MergeRequest
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("iid")]
+        public long Iid { get; set; }
+
+        [JsonProperty("project_id")]
+        public long ProjectId { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+
+        [JsonProperty("created_at")]
+        public DateTime CreatedAt { get; set; }
+
+        [JsonProperty("updated_at")]
+        public DateTime UpdatedAt { get; set; }
+
+        [JsonProperty("merged_by")]
+        public object MergedBy { get; set; }
+
+        [JsonProperty("merged_at")]
+        public object MergedAt { get; set; }
+
+        [JsonProperty("closed_by")]
+        public object ClosedBy { get; set; }
+
+        [JsonProperty("closed_at")]
+        public object ClosedAt { get; set; }
+
+        [JsonProperty("source_branch")]
+        public string SourceBranch { get; set; }
+
+        [JsonProperty("user_notes_count")]
+        public long UserNotesCount { get; set; }
+
+        [JsonProperty("upvotes")]
+        public long Upvotes { get; set; }
+
+        [JsonProperty("downvotes")]
+        public long Downvotes { get; set; }
+
+        [JsonProperty("author")]
+        public Author Author { get; set; }
+
+        [JsonProperty("assignees")]
+        public Author[] Assignees { get; set; }
+
+        [JsonProperty("assignee")]
+        public Author Assignee { get; set; }
+
+        [JsonProperty("source_project_id")]
+        public long SourceProjectId { get; set; }
+
+        [JsonProperty("target_project_id")]
+        public long TargetProjectId { get; set; }
+
+        [JsonProperty("labels")]
+        public string[] Labels { get; set; }
+
+        [JsonProperty("work_in_progress")]
+        public bool WorkInProgress { get; set; }
+
+        [JsonProperty("milestone")]
+        public object Milestone { get; set; }
+
+        [JsonProperty("merge_when_pipeline_succeeds")]
+        public bool MergeWhenPipelineSucceeds { get; set; }
+
+        [JsonProperty("merge_status")]
+        public string MergeStatus { get; set; }
+
+        [JsonProperty("sha")]
+        public string Sha { get; set; }
+
+        [JsonProperty("merge_commit_sha")]
+        public object MergeCommitSha { get; set; }
+
+        [JsonProperty("squash_commit_sha")]
+        public object SquashCommitSha { get; set; }
+
+        [JsonProperty("discussion_locked")]
+        public object DiscussionLocked { get; set; }
+
+        [JsonProperty("should_remove_source_branch")]
+        public object ShouldRemoveSourceBranch { get; set; }
+
+        [JsonProperty("force_remove_source_branch")]
+        public bool ForceRemoveSourceBranch { get; set; }
+
+        [JsonProperty("reference")]
+        public string Reference { get; set; }
+
+        [JsonProperty("references")]
+        public References References { get; set; }
+
+        [JsonProperty("web_url")]
+        public Uri WebUrl { get; set; }
+
+        [JsonProperty("time_stats")]
+        public TimeStats TimeStats { get; set; }
+
+        [JsonProperty("squash")]
+        public bool Squash { get; set; }
+
+        [JsonProperty("task_completion_status")]
+        public TaskCompletionStatus TaskCompletionStatus { get; set; }
+
+        [JsonProperty("has_conflicts")]
+        public bool HasConflicts { get; set; }
+
+        [JsonProperty("blocking_discussions_resolved")]
+        public bool BlockingDiscussionsResolved { get; set; }
+
+        [JsonProperty("approvals_before_merge")]
+        public object ApprovalsBeforeMerge { get; set; }
+    }
+
+    public partial class Author
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        [JsonProperty("avatar_url")]
+        public Uri AvatarUrl { get; set; }
+
+        [JsonProperty("web_url")]
+        public Uri WebUrl { get; set; }
+    }
+
+    public partial class References
+    {
+        [JsonProperty("short")]
+        public string Short { get; set; }
+
+        [JsonProperty("relative")]
+        public string Relative { get; set; }
+
+        [JsonProperty("full")]
+        public string Full { get; set; }
+    }
+
+    public partial class TaskCompletionStatus
+    {
+        [JsonProperty("count")]
+        public long Count { get; set; }
+
+        [JsonProperty("completed_count")]
+        public long CompletedCount { get; set; }
+    }
+
+    public partial class TimeStats
+    {
+        [JsonProperty("time_estimate")]
+        public long TimeEstimate { get; set; }
+
+        [JsonProperty("total_time_spent")]
+        public long TotalTimeSpent { get; set; }
+
+        [JsonProperty("human_time_estimate")]
+        public object HumanTimeEstimate { get; set; }
+
+        [JsonProperty("human_total_time_spent")]
+        public object HumanTotalTimeSpent { get; set; }
+    }
+
+
+
+}
